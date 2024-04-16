c966d122bc55efb71b09688d9ed24e38b2781822
Ian <iandkaneshiro@gmail.com>
2024-04-15 18:33:39 -0700

    I made stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,24 @@
 import styles from "./page.module.css";
 
+type Poop = {
+  wet: boolean;
+  name: string;
+  age: number;
+};
+
+const poop: Poop = {
+  wet: true,
+  name: "Big wet poo",
+  age: 12,
+};
+
 export default function Home() {
   return (
     <main className={styles.main}>
       <h1>Welcome to KanKare</h1>
+      <p>Name of poo {poop.name}</p>
+      <p>Is Wet {poop.wet ? "SUPAH WET" : "Dry as the sahara"}</p>
+      <p>How long it been sittin'? {poop.age} years</p>
     </main>
   );
 }
