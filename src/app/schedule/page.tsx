c1f41cc62021e3af4e25a6d308deb880a8911f48
Jeanette <haltonjeanette@gmail.com>
2024-05-02 16:34:04 -0700

    Planning functionality of schedule

---
+++ src/app/schedule/page.tsx
@@ -0,0 +1,12 @@
+/*
+Some things I would like to see in the calendar:
+• scheduled shifts
+• open shifts
+• events:
+    -family that might be out of town
+    -carers that will be unavailable to take on shifts
+    -doctor's appointments
+• could see a rough overview of the day? click on day to get a more detailed view
+• the calendar should show who is scheduled to cover each shift. anyone with access to the calendar should
+    be able to see that.
+*/
