2a7ced5d439f3de01afd08a5de8a350e6a54efe4
Jeanette <haltonjeanette@gmail.com>
2024-05-06 16:02:04 -0700

    Begin schedule

--- src/app/schedule/page.tsx
+++ src/app/schedule/page.tsx
@@ -9,4 +9,15 @@ Some things I would like to see in the calendar:
 • could see a rough overview of the day? click on day to get a more detailed view
 • the calendar should show who is scheduled to cover each shift. anyone with access to the calendar should
     be able to see that.
+• i would like to see available shifts on the calendar and for users to be able to click on that day to have
+    the option to pick up the shift
 */
+
+//npm install react-calendar
+import Calendar from "react-calendar";
+
+const Schedule = () => {
+  return <p>Calendariooo</p>;
+};
+
+export default Schedule;
