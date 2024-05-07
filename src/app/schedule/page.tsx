115f3d29f664951ace2c6153c6f52b09105c2c1a
Jeanette <haltonjeanette@gmail.com>
2024-05-06 18:03:50 -0700

    I think I made this more of a mess. Calendar isn't attached to any kind of data I guess??

--- src/app/schedule/page.tsx
+++ src/app/schedule/page.tsx
@@ -14,19 +14,35 @@ Some things I would like to see in the calendar:
 */
 
 //npm install react-calendar
+//npm install react-date-picker
+"use client";
+
 import React, { useState } from "react";
 import Calendar from "react-calendar";
+
 import "react-calendar/dist/Calendar.css";
 
 const Schedule = () => {
+  const [amShift, setAmShift] = useState(null); //amShift.userId would be the user assigned to this shift.
+  const [pmShift, setPmShift] = useState(null);
+  const [overnightShift, setOvernightShift] = useState(null);
   return (
     <div>
       <Calendar calendarType="gregory" />
+
       <div>
         <p>
           • Need user. If userId for shift === null, something needs to signal
           that there is an uncovered shift
         </p>
+        <p>• AM/PM/OVERNIGHT</p>
+        <p style={{ color: "red" }}>⚠</p>
+      </div>
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <p>Day at a glance</p>
+        <span>AM Shift</span>
+        <span>PM Shift</span>
+        <span>Overnight Shift</span>
       </div>
     </div>
   );
