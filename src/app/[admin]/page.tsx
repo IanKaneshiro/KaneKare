af4c5bed53f10d82977e58055803f8afe3c1bbff
Ian <iandkaneshiro@gmail.com>
2024-06-10 19:10:47 -0700

    most recent status

---
+++ src/app/[admin]/page.tsx
@@ -0,0 +1,15 @@
+const AdminPage = () => {
+  return (
+    <div>
+      <h4>Users</h4>
+      <p>Ability to cancel people</p>
+      <p>Ability to set payrate</p>
+      <p>Ability to edit users</p>
+      <h4>Payroll </h4>
+      <p>choosing period of time && people</p>
+      <p>caregivers</p>
+    </div>
+  );
+};
+
+export default AdminPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,54 @@
 "use client";
 
 import { timePunch } from "@/actions";
+import { useRef } from "react";
+import styles from "./page.module.css";
 
 export default function Home() {
+  const dialogRef = useRef<HTMLDialogElement>(null);
+
+  function toggleDialog() {
+    if (!dialogRef.current) return;
+    dialogRef.current.hasAttribute("open")
+      ? dialogRef.current.close()
+      : dialogRef.current.showModal();
+  }
+
   return (
     <main>
-      <h1>Home</h1>
+      <div style={{ backgroundColor: "pink" }}>Navbar</div>
       <div>
-        <h2>Clock in</h2>
+        <h2>**Live clock**</h2>
         <div>
-          <button onClick={async () => await timePunch()}>Record Punch</button>
+          <button onClick={toggleDialog}>Record Punch</button>
         </div>
+        <dialog
+          ref={dialogRef}
+          className={styles.container}
+          onClick={(e) => {
+            if (e.currentTarget === e.target) {
+              toggleDialog();
+            }
+          }}
+        >
+          <div>
+            <h2>YOU LIKE CLOCK IN FOR WORK OR MAYBE CLOCK OUT???</h2>
+            <div className={styles.buttons}>
+              <button
+                className={styles.button}
+                onClick={async () => {
+                  await timePunch();
+                  toggleDialog();
+                }}
+              >
+                Yes
+              </button>
+              <button className={styles.button} onClick={toggleDialog}>
+                Close
+              </button>
+            </div>
+          </div>
+        </dialog>
       </div>
     </main>
   );
