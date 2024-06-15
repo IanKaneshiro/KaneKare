9e2cb902ecc7fed0e9359f704080ae87d646a887
Ian <iandkaneshiro@gmail.com>
2024-06-15 15:21:26 -0700

    button works, some lag

--- src/components/DialogComponent.tsx
+++ src/components/DialogComponent.tsx
@@ -7,6 +7,8 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 
+import { useState, useEffect } from "react";
+
 import { ToastMessage } from "@/types/types";
 
 import { useToast } from "./ui/use-toast";
@@ -15,30 +17,46 @@ import { Button } from "./ui/button";
 
 import { timePunch } from "@/actions/shiftActions";
 
-import ClockInButton from "./ClockInButton";
+import { useUser } from "@clerk/nextjs";
 
 const DialogComponent = () => {
   const { toast } = useToast();
 
+  const { user } = useUser();
+
+  const [isClockedIn, setIsClockedIn] = useState<boolean | {} | null>(false);
+
+  useEffect(() => {
+    if (user?.unsafeMetadata.isClockedIn !== undefined) {
+      setIsClockedIn(user.unsafeMetadata.isClockedIn);
+    }
+  }, [user]);
+
+  const handleTimePunch = async () => {
+    const timePunchMessage: ToastMessage = await timePunch();
+    toast(timePunchMessage);
+    if (isClockedIn !== undefined) {
+      setIsClockedIn(!isClockedIn);
+    }
+  };
+
   return (
     <Dialog>
       <DialogTrigger>
-        <ClockInButton />
+        <Button variant="destructive">
+          {isClockedIn ? "Clock Out" : "Clock In"}
+        </Button>
       </DialogTrigger>
       <DialogContent>
         <DialogHeader>
           <DialogTitle>Are you absolutely sure?</DialogTitle>
         </DialogHeader>
         <DialogFooter>
-          <Button
-            variant="destructive"
-            onClick={async () => {
-              let timePunchMessage: ToastMessage = await timePunch();
-              toast(timePunchMessage);
-            }}
-          >
-            <DialogTrigger>Yes</DialogTrigger>
-          </Button>
+          <DialogTrigger>
+            <Button variant="destructive" onClick={handleTimePunch}>
+              Yes
+            </Button>
+          </DialogTrigger>
         </DialogFooter>
       </DialogContent>
     </Dialog>
