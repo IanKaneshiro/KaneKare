8558229679f68bc37b918298639652dfb332f3b3
Jeanette <haltonjeanette@gmail.com>
2024-06-27 19:14:27 -0700

    Still failing-- unfulfilled promise

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -2,18 +2,31 @@ import mongoose from "mongoose";
 import { auth } from "@clerk/nextjs/server";
 import { getUsers } from "@/actions/shiftActions";
 
+const popUsers = async () => {
+  try {
+    const users = await getUsers();
+    if (users) {
+      return users;
+    }
+  } catch (e) {
+    console.error("Error retreiving users", e);
+  }
+};
+
+const usersList = popUsers();
+
 const AdminPage = () => {
-  const { userId } = auth();
-
-  const users = getUsers();
-
   return (
     <div>
       <div>
         <h4>Users</h4>
       </div>
       <div>
-        <h4>Actual page content. Need to move notes somewhere else maybe.</h4>
+        <ul>
+          {usersList.map((user) => (
+            <li key={user._id}>{user.firstName}</li>
+          ))}
+        </ul>
       </div>
     </div>
   );
