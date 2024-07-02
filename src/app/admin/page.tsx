f773cb621fcfa2dc90c842ce3c19a354216f8ddf
Jeanette <haltonjeanette@gmail.com>
2024-07-01 18:36:02 -0700

    Remove unnecessary function

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,15 +1,5 @@
 import { getUsers } from "@/actions/shiftActions";
 
-// const popUsers = async () => {
-//   try {
-//     const users = await getUsers();
-//     if (users) {
-//       return users;
-//     }
-//   } catch (e) {
-//     console.error("Error retreiving users", e);
-//   }
-// };
 
 const AdminPage = async () => {
   const usersList = await getUsers();
